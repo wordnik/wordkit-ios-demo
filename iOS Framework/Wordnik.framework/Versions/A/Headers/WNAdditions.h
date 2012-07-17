511997e88d9b101b74c44684df12bf6cd66757a1
Tony Tam <fehguy@gmail.com>
2012-07-16 21:53:50 -0700

    added header

---
+++ iOS Framework/Wordnik.framework/Versions/A/Headers/WNAdditions.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright Wordnik, Inc. 2010. All rights reserved.
+ */
+
+/**
+ * @internal
+ * @ingroup internal_api
+ * @defgroup additions API Extensions
+ *
+ * Provides a set of categories, classes, and functions that provide extensions and utility methods for Apple's
+ * standard frameworks.
+ */
+
+
+/**
+ * @internal
+ * @defgroup additions_cf Core Foundation
+ * @ingroup additions
+ *
+ * API additions and extensions for CoreFoundation.
+ */
+
+
+#import <Wordnik/WNCFAdditions.h>
+
+/**
+ * @internal
+ * @defgroup additions_uikit UIKit
+ * @ingroup additions
+ *
+ * API additions and extensions for UIKit.
+ */
+
+/**
+ * @internal
+ * @defgroup additions_foundation Foundation
+ * @ingroup additions
+ *
+ * API additions and extensions for Foundation.
+ */
+
+#import <Wordnik/WNFoundationAdditions.h>
+#import <Wordnik/NSRunLoop+WNAdditions.h>
+#import <Wordnik/NSString+WNAdditions.h>
+#import <Wordnik/NSURL+WNAdditions.h>
+#import <Wordnik/NSArray+WNAdditions.h>
+#import <Wordnik/NSDictionary+WNAdditions.h>
+
+#import <Wordnik/NSArray+WNTypedAccessors.h>
+#import <Wordnik/NSDictionary+WNTypedAccessors.h>
